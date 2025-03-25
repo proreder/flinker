e6270d1108affee7edcea1372e346060d93b9ee8
proreder <proreder@hotmail.com>
2025-03-25 12:48:45 +0100

    modificacion home.html y firebase-config.js

--- www/assets/js/firebase-config.js
+++ www/assets/js/firebase-config.js
@@ -1,17 +1,32 @@
-
 // Configuración de Firebase
-const firebaseConfig = {
-    apiKey: "AIzaSyBj7pQy-V_rh2zJ_atjUvXimljShHUFf8M",
-    authDomain: "flinker-8c0c9.web.app",
+// Importante las versiones para 'firebase-app.js' y para 'firebase-firestore.js' en ambos casos debes ser las mismas
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
+import {
+  getFirestore,
+  collection,
+  addDoc,
+  getDoc,
+  getDocs,
+  deleteDoc,
+  doc,
+  updateDoc,
+} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 
-    databaseURL: "https://flinker-8c0c9-default-rtdb.firebaseio.com/",
-    projectId: "TU_PROJECT_ID",
-    appId: "1:244533337010:android:297765d438bb5ae6e2e2ab",
+// Firebase configuration (replace with your actual project details)
+const firebaseConfig = {
+    apiKey: "AIzaSyDSzQsL2S7rJPl6FQPmaDpZoxBkeygO76s",
+    authDomain: "my-flinker-project-id.firebaseapp.com",
+    projectId: "my-flinker-project-id",
+    storageBucket: "my-flinker-project-id.firebasestorage.app",
+    messagingSenderId: "984525672081",
+    appId: "1:984525672081:web:a181cc7679a5cef6edf930",
+    measurementId: "G-55MQ4SSF00"
 };
 
-// Inicializar Firebase
-const app = firebase.initializeApp(firebaseConfig);
-const database = firebase.database();
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
+// Inicializa Cloud Firestore y obtén una referencia al servicio.
+export const db = getFirestore(app);
 
 console.log("Firebase inicializado correctamente.");
 
