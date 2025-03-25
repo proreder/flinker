f9ceabf6a51f13ddd9eb2569180ca046f3074184
proreder <proreder@hotmail.com>
2025-03-25 13:40:13 +0100

    modificacion home.html

--- www/assets/js/firebase-functions.js
+++ www/assets/js/firebase-functions.js
@@ -12,3 +12,19 @@ function enviarMensaje() {
         console.error("Error al enviar el mensaje:", error);
     });
 }
+
+/**
+ * ***************************************************************************
+ * Acciones para el TODO (Create, Read, Update y Delete)
+ * Una colección es equivalente a una tabla en una base de datos relacional.
+ * Un Documento es equivalente a un registro en una tabla en una base de datos.
+ * ***************************************************************************
+ */
+const coleccion = "tbl_usuarios";
+
+// Esta función agrega un nuevo usuario a una colección en Firestore, con los detalles proporcionados como nombre, correo, edad, sexo, email, imagen.
+export const addUsuario = (nombre, correo, edad, sexo, email, imagen) =>
+  addDoc(collection(db, coleccion), { nombre, correo, edad, sexo, email, imagen });
+
+// Esta función obtiene todos los documentos de una colección de usuarios en Firestore y devuelve una promesa que se resuelve con los datos de esos documentos.
+export const getUsuariosCollection = () => getDocs(collection(db, coleccion));
