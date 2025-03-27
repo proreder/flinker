b7f79f6b4987684888372ad70d9b80a8caa8bb7e
proreder <proreder@users.noreply.github.com>
2025-03-27 03:06:17 +0100

    sss

--- www/assets/js/firebase-config.js
+++ www/assets/js/firebase-config.js
@@ -1,51 +1,60 @@
+//import "./firebase/firestore.js";
+//import "./firebase/firebase.js";
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDoc,
-  getDocs,
-  deleteDoc,
-  doc,
-  updateDoc,
-} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
-
-//cargamos las funciones de ususrios
-import {
-  addUsuario,
-  getUsuariosCollection,
- } from "./firebase-functions.js";
 // Configuración de Firebase
 // Importante las versiones para 'firebase-app.js' y para 'firebase-firestore.js' en ambos casos debes ser las mismas
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-app.js";
+import { getFirestore } from ".https://www.gstatic.com/firebasejs/11.5.0/firebase-firestore.js";
+import { getAnalytics } from "https://www.gstatic.com/firebasejs/11.5.0/firebase-analytics.js";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
 
-
-// Firebase configuration (replace with your actual project details)
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyDSzQsL2S7rJPl6FQPmaDpZoxBkeygO76s",
-    authDomain: "my-flinker-project-id.firebaseapp.com",
-    projectId: "my-flinker-project-id",
-    storageBucket: "my-flinker-project-id.firebasestorage.app",
-    messagingSenderId: "984525672081",
-    appId: "1:984525672081:web:a181cc7679a5cef6edf930",
-    measurementId: "G-55MQ4SSF00"
+  apiKey: "AIzaSyDSzQsL2S7rJPl6FQPmaDpZoxBkeygO76s",
+  authDomain: "my-flinker-project-id.firebaseapp.com",
+  databaseURL: "https://my-flinker-project-id-default-rtdb.europe-west1.firebasedatabase.app",
+  projectId: "my-flinker-project-id",
+  storageBucket: "my-flinker-project-id.firebasestorage.app",
+  messagingSenderId: "984525672081",
+  appId: "1:984525672081:web:a181cc7679a5cef6edf930",
+  measurementId: "G-55MQ4SSF00"
 };
 
+console.log("antes");
 // Initialize Firebase
-export const app = initializeApp(firebaseConfig);
-// Inicializa Cloud Firestore y obtén una referencia al servicio.
-export const db = getFirestore(app);
-
-// Esta función agrega un nuevo usuario a una colección en Firestore, con los detalles proporcionados como nombre, correo, edad, sexo, email, imagen.
-export const addUsuario = (nombre, correo, edad, sexo, email, imagen) =>
-  addDoc(collection(db, collection), { nombre, correo, edad, sexo, email, imagen });
-
-// Esta función obtiene todos los documentos de una colección de usuarios en Firestore y devuelve una promesa que se resuelve con los datos de esos documentos.
-export const getUsuariosCollection = () => getDocs(collection(db, collection));
-
-
-
+const app=initializeApp(firebaseConfig);
 console.log("Firebase inicializado correctamente.");
+//const analytics = getAnalytics(app);
+console.log("Firebase .");
+let db;
+try {
+  db = getFirestore(app);
+   console.log("Firestore instance:", db);
+} catch (error) {
+  console.error("Error initializing Firestore:", error.message);
+  console.error("Error Stack:", error.stack);
+  console.error("Error Details:", error);
+}
+
+console.log("Firebase .");
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
--- www/assets/js/firebase-functions.js
+++ www/assets/js/firebase-functions.js
@@ -1,33 +1,6 @@
 
-// Configuración de Firebase
-// Importante las versiones para 'firebase-app.js' y para 'firebase-firestore.js' en ambos casos debes ser las mismas
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-app.js";
-import {
-  getFirestore,
-  collection,
-  addDoc,
-  getDoc,
-  getDocs,
-  deleteDoc,
-  doc,
-  updateDoc,
-} from "https://www.gstatic.com/firebasejs/10.4.0/firebase-firestore.js";
 
-// Función para enviar un mensaje a Firebase
-function enviarMensaje() {
-    const mensaje = document.getElementById("message").value;
-    const refMensajes = database.ref('mensajes');
-    refMensajes.push({
-        usuario: "Usuario1",
-        mensaje: mensaje,
-        hora: new Date().toLocaleString(),
-    }).then(() => {
-        console.log("Mensaje enviado correctamente.");
-    }).catch((error) => {
-        console.error("Error al enviar el mensaje:", error);
-    });
-}
-
+//import { collection, addDoc } from "./firebase/firestore.js"; 
 /**
  * ***************************************************************************
  * Acciones para el TODO (Create, Read, Update y Delete)
@@ -35,49 +8,9 @@ function enviarMensaje() {
  * Un Documento es equivalente a un registro en una tabla en una base de datos.
  * ***************************************************************************
  */
-const coleccion = "tbl_usuarios";
-
-// Esta función agrega un nuevo usuario a una colección en Firestore, con los detalles proporcionados como nombre, correo, edad, sexo, email, imagen.
-export const addUsuario = (nombre, correo, edad, sexo, email, imagen) =>
-  addDoc(collection(db, coleccion), { nombre, correo, edad, sexo, email, imagen });
-
-// Esta función obtiene todos los documentos de una colección de usuarios en Firestore y devuelve una promesa que se resuelve con los datos de esos documentos.
-export const getUsuariosCollection = () => getDocs(collection(db, coleccion));
+const coleccion = "usuarios";
 
 
-//window.addEventListener("DOMContentLoaded", mostrarEmpleadosEnHTML);
-
-/**
- * Función para agregar un nuevo empleado
- */
-window.addNuevoUsuario = async function (event) {
-  event.preventDefault();
-
-  const formulario = document.querySelector("#formularioAlta");
-  const formData = new FormData(formulario);
-
-  // Convertir FormData a un objeto JSON
-  const formDataJSON = {};
-  formData.forEach((value, key) => {
-    formDataJSON[key] = value;
-  });
-
-  const { nombre, correo, edad, sexo, imgInp, aficion1 } = formDataJSON;
-
-  try {
-    await addUsuario(nombre, correo, edad, sexo, imgInp, aficion1);
-    formulario.reset();
-    /*
-    setTimeout(() => {
-      $("#agregarEmpleadoModal").css("opacity", "");
-      $("#agregarEmpleadoModal").modal("hide");
-    }, 300);
-    */
-    window.mostrarAlerta({ tipoToast: "success", mensaje: "¡Usuario guardado!" });
-  } catch (error) {
-    console.log(error);
-  }
-};
 
 
 /**
